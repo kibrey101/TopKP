677535015fdcd62d2b336e2502152c8cce0fb50d
kibrey101 <kibtsegai@gmail.com>
2018-04-23 00:10:57 +0300

    Add project files.

---
+++ TopHundred.API/Entities/IcoItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TopHundred.API.Entities
+{
+    public enum IcoTypeEnumeration { PreIco = 1000, ActiveIco, UpcomingIco, PastIco }
+    public class IcoItem
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required, MaxLength(50)]
+        public string Name { get; set; }
+        [Required, MaxLength(500)]
+        public string Description { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public string PriceCurrency{ get; set; }
+        [Required, MaxLength(10)]
+        public string Symbol { get; set; }
+        [Required]
+        public string IconLink { get; set; }
+        [Required]
+        public IcoTypeEnumeration IcoTypeEnumeration { get; set; }  
+        [Required, MaxLength(20)]
+        public string Status { get; set; }
+        [Required]
+        public DateTime StartTime { get; set; }
+        [Required]
+        public DateTime EndTime { get; set; }
+        [Required]
+        public string WhitePaper { get; set; }
+        public User User { get; set; }
+        public int TotalTokenCount { get; set; }
+        public int Rank { get; set; }          
+        public IEnumerable<string> IcoLinks { get; set; } = new List<string>();
+
+    }
+}
---
+++ TopHundred.API/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TopHundred.API.Entities
+{
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+
+    }
+}
